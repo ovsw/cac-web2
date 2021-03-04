13cefebb2d791ec1977703a8a500d609f881c594
Ovi Savescu <ovi@ovswebsites.com>
2021-03-04 18:02:22 +0200

    initial commit

---
+++ client-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  sanity: {
+    projectId: process.env.SANITY_PROJECT_ID || 'lwnx6aqb',
+    dataset: process.env.SANITY_DATASET || 'production'
+  }
+}
---
+++ src/_data/sitePages.js
@@ -0,0 +1,43 @@
+const groq = require('groq')
+const client = require('../utils/sanityClient')
+const overlayDrafts = require('../utils/overlayDrafts')
+const hasToken = !!client.config().token
+
+
+function generatePageWithSeo (page) {
+
+  const deoDescription = page.content.seoDescription != '' ? page.content.seo.seoDescription :  'excerpt'
+  // const seoDescription = page.content.seo.description || pageBOdyTextTruncated
+
+  return {
+    ...page,
+    seoDescription: deoDescription
+  }
+}
+
+
+module.exports =  async function() {
+  const sanityResponse = await client.fetch(groq`
+  *[_type == "page"]{
+    ...,
+    content {
+      ...,
+  		'seoTitle': coalesce(seo.title, title),
+			'seoDescription': coalesce(seo.description, ''),
+			sections[] {
+        ...,
+        reusableSection->{
+          ...
+        }
+      }
+    }
+  }
+  `).catch(err => console.error(err))
+
+  
+  const reducedDocs = overlayDrafts(hasToken, sanityResponse)
+  
+  const prepareItems = reducedDocs.map(generatePageWithSeo)
+
+  return prepareItems
+}
